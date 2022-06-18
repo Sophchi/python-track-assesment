b23f22cbd746175c0ec90d2badf11c11ad4dd8ca
sophia chioma <sophiachioma@gmail.com>
2022-06-18 22:20:07 +0100

    python code to check if a digit is prime or not

--- src/app.py
+++ src/app.py
@@ -4,9 +4,18 @@ import os
 
 def prime(s):
     # your code goes here
+    for num in range(2,s):
+        if s % num == 0:
+            return f' The value {s} is not a prime number'
+        else:
+            return f' The value {s} is a prime number'
+
+s = int(input("enter a value: "))
 
 def solution(s):
     return prime(s)
+
+print(solution(s))
 
 
 if __name__ == "__main__":
@@ -14,3 +23,5 @@ if __name__ == "__main__":
         sys.exit(os.error("Argument required"))
 
     print(solution(sys.argv[1]))
+
+print(type(sys))
